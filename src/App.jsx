1c4eb830d2a66f5695659ce5c7562820640d96e4
Marco CARPENTIER <marcarp@protonmail.com>
2024-10-27 17:24:15 +0100

    Using data to show with state system

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcepts.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState('Please click this button');
+    const [selectedTopic, setSelectedTopic] = useState('components');
 
     function handleSelect (selectedButton) {
         setSelectedTopic(selectedButton);
-        console.log(selectedTopic);
 	}
 
     console.log('APP COMPONENT EXECUTING');
@@ -30,13 +29,21 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton onSelect={()=>handleSelect('Components')}>Components</TabButton>
-                        <TabButton onSelect={()=>handleSelect('JSX')}>JSX</TabButton>
-                        <TabButton onSelect={()=>handleSelect('Props')}>Props</TabButton>
-                        <TabButton onSelect={()=>handleSelect('State')}>State</TabButton>
+                        <TabButton onSelect={()=>handleSelect('components')}>Components</TabButton>
+                        <TabButton onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
+                        <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
                     </menu>
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>{EXAMPLES[selectedTopic].code}</code>
+                        </pre>
+                    </div>
                 </section>
-                {selectedTopic}
+                {selectedTopic.title}
+                {selectedTopic.description}
             </main>
         </div>
   );
