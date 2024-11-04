e017ccb0a994d7a7b259084c7706a3fd18e47cad
Marco CARPENTIER <marcarp@protonmail.com>
2024-11-04 09:23:47 +0100

    Custom component example - Problem with non following attributes

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,13 +1,13 @@
 import { CORE_CONCEPTS } from '../data';
+import Section from './Section';
 import CoreConcept from './CoreConcept';
 
 export default function CoreConcepts() {
     return (
-        <section id="core-concepts">
-            <h2>Core Concepts</h2>
-                <ul>
-                    {CORE_CONCEPTS.map((conceptItem)=> <CoreConcept key={conceptItem.title} {...conceptItem} />)}
-                </ul>
-        </section>
+        <Section id="core-concepts" title="Core Concepts">
+            <ul>
+                {CORE_CONCEPTS.map((conceptItem)=> <CoreConcept key={conceptItem.title} {...conceptItem} />)}
+            </ul>
+        </Section>
     )
 }
