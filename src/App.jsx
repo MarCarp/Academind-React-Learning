60861fb5f0dee0b14f426fe1f6f5373c6d041c05
Marco CARPENTIER <marcarp@protonmail.com>
2024-11-17 17:11:59 +0100

    Tic Tac Toe Project - Game Board with multi-dimensional lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Player from './components/Player'
+import GameBoard from './components/GameBoard'
+
 function App() {
   return (
     <main>
@@ -8,7 +10,7 @@ function App() {
           <Player initialName="Player 2" symbol="O" />
         </ol>
 
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
